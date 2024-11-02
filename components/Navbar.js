0c4bbe4455fa6785a6e5339e71a10b2ae849fa05
paiabhishekpai <abhishekupai1124@gmail.com>
2024-11-02 18:47:09 +0530

    initialising a navbar and footer

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+  <nav>
+    <ul className="flex gap-3">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+  </nav>
+  )
+};
+
+export default Navbar;
